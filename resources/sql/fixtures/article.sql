dba76602fba2407dcc26529d4495fe5d9b725d05
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2019-07-30 09:40:20 +0200

    feature #19: improve article fixtures and add relations

--- resources/sql/fixtures/article.sql
+++ resources/sql/fixtures/article.sql
@@ -9,7 +9,9 @@ declare
     }
     $tags$;
 begin
+    delete from article_relations;
     delete from article;
+
     insert into article (version_id, created_by_id, title, annonymous, content, description, tags)
     select
         uuid_generate_v4(),
@@ -20,6 +22,14 @@ begin
         'description' || row_number() over (),
         _tags[(row_number() over () % 5):(row_number() over () % 9)]
     from citizen z;
+
+    insert into article_relations (source_id, target_id, created_by_id)
+    select
+        src.id,
+        dest.id,
+        src.created_by_id
+    from (select *, row_number() over () rn from article, lateral generate_series(1, 5) g) src
+    join (select *, row_number() over () +5 rn from article) dest using (rn);
 
     raise notice 'article fixtures done';
 end;
