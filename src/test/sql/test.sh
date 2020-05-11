b497c61cfcf1fc08b86e37539dc0cb0edd879eac
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2020-05-12 00:58:52 +0200

    Create bash script to execute SQL tests

--- src/test/sql/test.sh
+++ src/test/sql/test.sh
@@ -1,33 +1,43 @@
 #!/bin/bash
 
-PS3='Please enter your choice: '
-options=("RESET DB" "All" "article" "citizen" "comment" "constitution" "follow" "opinion" "user" "vote" "workgroup" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "RESET DB")
-            cat \
-              ../../main/resources/sql/migrations/*.down.sql \
-              ../../main/resources/sql/migrations/*.up.sql > ./allSQL.sql
-            docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
-            rm ./allSQL.sql
-            break;;
-        "All")
-            cat ../../main/resources/sql/functions/*/*.sql \
-              ./fixtures/*.sql \
-              ./*.sql > ./allSQL.sql
-            docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
-            rm ./allSQL.sql
-            break;;
-        "Quit")
-            break;;
-        *) echo "Start tests $opt"
-            cat ../../main/resources/sql/functions/*/*.sql \
-              ./fixtures/*.sql \
-              ./"$opt".sql > ./allSQL.sql
-            docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
-            rm ./allSQL.sql
-            break;;
-    esac
-done
+options=("All" "article" "citizen" "comment" "constitution" "follow" "opinion" "user" "vote" "workgroup" "RESET DB" "Quit")
+if [ -z "$1" ]; then
+  PS3='Please enter your choice: '
+  select ch in "${options[@]}"
+  do
+    opt=$ch
+    break
+  done
+else
+  opt=${options[${1}-1]}
+fi
 
+case $opt in
+  "RESET DB")
+      awk 'FNR==1{print "--------------------"}1' \
+        ../../main/resources/sql/migrations/*.down.sql \
+        ../../main/resources/sql/migrations/*.up.sql > ./allSQL.sql
+      docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
+      rm ./allSQL.sql
+      ;;
+  "All")
+      echo "Start ALL tests"
+      awk 'FNR==1{print "--------------------"}1' \
+        ../../main/resources/sql/functions/*/*.sql \
+        ./fixtures/*.sql \
+        ./*.sql > ./allSQL.sql
+      docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
+      rm ./allSQL.sql
+      ;;
+  "Quit")
+      ;;
+  *)
+    echo "Start tests $opt"
+    awk 'FNR==1{print "--------------------"}1' \
+      ../../main/resources/sql/functions/*/*.sql \
+      ./fixtures/*.sql \
+      ./"$opt".sql > ./allSQL.sql
+    docker exec -i postgresql_dc-project psql test test -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
+    rm ./allSQL.sql
+    ;;
+esac
