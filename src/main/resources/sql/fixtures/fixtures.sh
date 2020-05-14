bd123d03e97a05da6589ef8e99c5bdc5c1faefa4
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2020-05-14 13:46:55 +0200

    Fix fixtures script

--- src/main/resources/sql/fixtures/fixtures.sh
+++ src/main/resources/sql/fixtures/fixtures.sh
@@ -1,6 +1,5 @@
-#bin/bash
-echo "Start fixtures"
-awk 'FNR==1{print "--------------------"}1' ./*.sql > ./allSQL.sql
+#!/usr/bin/env bash
+awk 'FNR==1{print "--------------------"}1' `dirname $0`/*.sql > ./allSQL.sql
 docker exec -i postgresql_dc-project psql dc-project dc-project -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
 rm ./allSQL.sql
 echo "End fixtures"
