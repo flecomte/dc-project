f7b6cc4eb3322df3a5fe59ab249efb383fbb7104
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2021-02-13 01:28:05 +0100

    fix DB test script

--- src/main/resources/sql/resetDB.sh
+++ src/main/resources/sql/resetDB.sh
@@ -4,6 +4,6 @@ awk 'FNR==1{print "--------------------"}1' \
   ./migrations/*.down.sql \
   ./migrations/*.up.sql \
   ./functions/*/*.sql > ./allSQL.sql
-docker exec -i postgresql_dc-project psql dc-project dc-project -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
+docker exec -i dc-project_postgresql psql dc-project dc-project -q -b -v "ON_ERROR_STOP=1" < ./allSQL.sql
 rm ./allSQL.sql
 echo "End reset"
