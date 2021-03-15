189aa8d5493c5f9516159b2749ef4930d41deeac
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2021-03-15 02:02:26 +0100

    Test openapi schema response of Login

--- src/main/kotlin/fr/dcproject/component/auth/routes/Login.kt
+++ src/main/kotlin/fr/dcproject/component/auth/routes/Login.kt
@@ -7,10 +7,12 @@ import fr.dcproject.component.auth.jwt.makeToken
 import fr.dcproject.component.auth.routes.Login.LoginRequest.Input
 import io.ktor.application.call
 import io.ktor.auth.UserPasswordCredential
+import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.post
+import io.ktor.request.accept
 import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.Route
@@ -32,8 +34,16 @@ object Login {
                     UserPasswordCredential(username, password)
                 }
 
-                userRepo.findByCredentials(credentials)?.let { user ->
-                    call.respondText(user.makeToken())
+                userRepo.findByCredentials(credentials)?.makeToken()?.let { token ->
+                    if (call.request.accept() == ContentType.Application.Json.toString()) {
+                        call.respond(
+                            object {
+                                val token: String = token
+                            }
+                        )
+                    } else {
+                        call.respondText(token)
+                    }
                 } ?: call.respond(HttpStatusCode.BadRequest, "Username not exist or password is wrong")
             } catch (e: MismatchedInputException) {
                 call.respond(HttpStatusCode.BadRequest, "You must be send name and password to the request")
