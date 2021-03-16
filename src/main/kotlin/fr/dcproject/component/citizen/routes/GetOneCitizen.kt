5fce37f26904fdbc87896cc21ed9add8a9a4b25a
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2021-03-17 00:28:01 +0100

    Test openapi schema of /citizens/{id}

--- src/main/kotlin/fr/dcproject/component/citizen/routes/GetOneCitizen.kt
+++ src/main/kotlin/fr/dcproject/component/citizen/routes/GetOneCitizen.kt
@@ -1,6 +1,7 @@
 package fr.dcproject.component.citizen.routes
 
 import fr.dcproject.common.security.assert
+import fr.dcproject.component.auth.citizen
 import fr.dcproject.component.auth.citizenOrNull
 import fr.dcproject.component.citizen.CitizenAccessControl
 import fr.dcproject.component.citizen.database.CitizenRef
@@ -12,6 +13,8 @@ import io.ktor.locations.Location
 import io.ktor.locations.get
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 import java.util.UUID
 
 @KtorExperimentalLocationsAPI
@@ -26,7 +29,25 @@ object GetOneCitizen {
             val citizen = citizenRepository.findById(it.citizen.id) ?: throw NotFoundException("Citizen not found ${it.citizen.id}")
             ac.assert { canView(citizen, citizenOrNull) }
 
-            call.respond(it.citizen)
+            call.respond(
+                object {
+                    val id: UUID = citizen.id
+                    val name: Any = citizen.name.let { n ->
+                        object {
+                            val firstName: String = n.firstName
+                            val lastName: String = n.lastName
+                        }
+                    }
+                    val email: String = citizen.email
+                    val birthday: String = citizen.birthday.run {
+                        toString(ISODateTimeFormat.date())
+                    }
+                    val createdAt: DateTime = citizen.createdAt
+                    val user: Any = object {
+                        val username: String = citizen.user.username
+                    }
+                }
+            )
         }
     }
 }
