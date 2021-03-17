78a6cc417c0ee24fc7e2cdc3d6ce532143cea703
Fabrice Lecomte <fabrice.lecomte.be@gmail.com>
2021-03-17 22:07:14 +0100

    Test openapi schema of POST/GET /constitutions/{constitution}/comments GET /citizens/{citizen}/comments/constitutions

--- src/main/kotlin/fr/dcproject/component/comment/constitution/routes/CreateConstitutionComment.kt
+++ src/main/kotlin/fr/dcproject/component/comment/constitution/routes/CreateConstitutionComment.kt
@@ -1,9 +1,11 @@
 package fr.dcproject.component.comment.constitution.routes
 
 import fr.dcproject.common.security.assert
+import fr.dcproject.common.utils.receiveOrBadRequest
 import fr.dcproject.component.auth.citizen
 import fr.dcproject.component.auth.citizenOrNull
 import fr.dcproject.component.comment.constitution.database.CommentConstitutionRepository
+import fr.dcproject.component.comment.constitution.routes.CreateConstitutionComment.CreateConstitutionCommentRequest.Input
 import fr.dcproject.component.comment.generic.CommentAccessControl
 import fr.dcproject.component.comment.generic.database.CommentForUpdate
 import fr.dcproject.component.constitution.database.ConstitutionRef
@@ -12,9 +14,9 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.post
-import io.ktor.request.receiveText
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import org.joda.time.DateTime
 import java.util.UUID
 
 @KtorExperimentalLocationsAPI
@@ -22,20 +24,66 @@ object CreateConstitutionComment {
     @Location("/constitutions/{constitution}/comments")
     class CreateConstitutionCommentRequest(constitution: UUID) {
         val constitution = ConstitutionRef(constitution)
+        class Input(val content: String)
     }
 
     fun Route.createConstitutionComment(repo: CommentConstitutionRepository, ac: CommentAccessControl) {
         post<CreateConstitutionCommentRequest> {
-            val content = call.receiveText()
-            val comment = CommentForUpdate(
-                target = it.constitution,
-                createdBy = citizen,
-                content = content
-            )
-            ac.assert { canCreate(comment, citizenOrNull) }
-            repo.comment(comment)
+            call.receiveOrBadRequest<Input>().run {
+                CommentForUpdate(
+                    target = it.constitution,
+                    createdBy = citizen,
+                    content = content
+                )
+            }.let { comment ->
+                ac.assert { canCreate(comment, citizenOrNull) }
+                repo.comment(comment)
 
-            call.respond(HttpStatusCode.Created, comment)
+                call.respond(
+                    HttpStatusCode.Created,
+                    object {
+                        val id: UUID = comment.id
+                        val content: String = comment.content
+                        val childrenCount: Int = 0
+                        val createdAt: DateTime = comment.createdAt
+                        val parent: Any? = comment.parent?.let { p ->
+                            object {
+                                val id: UUID = p.id
+                                val reference: String = p.reference
+                            }
+                        }
+                        val target: Any = comment.target.let { t ->
+                            object {
+                                val id: UUID = t.id
+                                val reference: String = t.reference
+                            }
+                        }
+                        val createdBy: Any = comment.createdBy.let { c ->
+                            object {
+                                val id: UUID = c.id
+                                val name: Any = c.name.let { n ->
+                                    object {
+                                        val firstName: String = n.firstName
+                                        val lastName: String = n.lastName
+                                    }
+                                }
+                                val user: Any = c.user.let { u ->
+                                    object {
+                                        val username: String = u.username
+                                    }
+                                }
+                            }
+                        }
+                        val votes: Any = object {
+                            val up: Int = 0
+                            val neutral: Int = 0
+                            val down: Int = 0
+                            val total: Int = 0
+                            val score: Int = 0
+                        }
+                    }
+                )
+            }
         }
     }
 }
--- src/main/kotlin/fr/dcproject/component/comment/constitution/routes/GetConstitutionComment.kt
+++ src/main/kotlin/fr/dcproject/component/comment/constitution/routes/GetConstitutionComment.kt
@@ -1,5 +1,6 @@
 package fr.dcproject.component.comment.constitution.routes
 
+import fr.dcproject.common.dto.toOutput
 import fr.dcproject.common.security.assert
 import fr.dcproject.component.auth.citizenOrNull
 import fr.dcproject.component.comment.constitution.database.CommentConstitutionRepository
@@ -12,6 +13,7 @@ import io.ktor.locations.Location
 import io.ktor.locations.get
 import io.ktor.response.respond
 import io.ktor.routing.Route
+import org.joda.time.DateTime
 import java.util.UUID
 
 @KtorExperimentalLocationsAPI
@@ -25,7 +27,54 @@ object GetConstitutionComment {
         get<GetConstitutionCommentRequest> {
             val comments = repo.findByTarget(it.constitution)
             ac.assert { canView(comments.result, citizenOrNull) }
-            call.respond(HttpStatusCode.OK, comments)
+            call.respond(
+                HttpStatusCode.OK,
+                comments.toOutput { comment ->
+                    object {
+                        val id: UUID = comment.id
+                        val content: String = comment.content
+                        val childrenCount: Int = comment.childrenCount ?: 0
+                        val createdAt: DateTime = comment.createdAt
+                        val parent: Any? = comment.parent?.let { p ->
+                            object {
+                                val id: UUID = p.id
+                                val reference: String = p.reference
+                            }
+                        }
+                        val target: Any = comment.target.let { t ->
+                            object {
+                                val id: UUID = t.id
+                                val reference: String = t.reference
+                            }
+                        }
+                        val createdBy: Any = comment.createdBy.let { c ->
+                            object {
+                                val id: UUID = c.id
+                                val name: Any = c.name.let { n ->
+                                    object {
+                                        val firstName: String = n.firstName
+                                        val lastName: String = n.lastName
+                                    }
+                                }
+                                val user: Any = c.user.let { u ->
+                                    object {
+                                        val username: String = u.username
+                                    }
+                                }
+                            }
+                        }
+                        val votes: Any = comment.votes.let { v ->
+                            object {
+                                val up: Int = v.up
+                                val neutral: Int = v.neutral
+                                val down: Int = v.down
+                                val total: Int = v.total
+                                val score: Int = v.score
+                            }
+                        }
+                    }
+                }
+            )
         }
     }
 }
